9c8f5160d18d959ad3df43bee20bc3767d44f1fa
rumkin <hello@rumk.in>
2013-06-21 18:41:01 +0400

    Examples added

---
+++ examples/empty/main.js
@@ -0,0 +1,31 @@
+var Shaft = require('../src/shaft');
+
+var app = Shaft.create({
+	mode        : 'development',
+	basedir     : __dirname,
+	views       : 'views',
+	controllers : 'controllers',
+	statics     : 'ui:public',
+	service_dir : 'services',
+	services    : {
+		jade : true
+	}
+});
+
+app.on('jade.init', function(jade) {
+	console.log('jade is ready!!!');
+});
+
+app.start({
+	development : function(app, express) {
+
+	},
+
+	production : function(app, express) {
+
+	}
+});
+
+app.on('error', function(err) {
+	console.log('Error', err);
+});
