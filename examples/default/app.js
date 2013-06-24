2b93a919382cad2c5ca93834946db56f08411124
rumkin <hello@rumk.in>
2013-06-24 15:48:16 +0400

    Default project updated

--- examples/default/app.js
+++ examples/default/app.js
@@ -1,7 +1,25 @@
-var Shaft = require('shaft-js');
+var Shaft  = require('shaft-js');
+var Getopt = require('node-getopt');
+
+var getopt, options;
+
+getopt = new Getopt([
+	['H', 'host=ARG', 'hostname or ip'],
+	['p', 'port=ARG', 'port number'],
+	['h', 'help', 'show help']
+]).bindHelp();
+
+getopt.setHelp('Usage:\n[[OPTIONS]]');
+
+options = getopt.parseSystem().options;
+if (options.help) {
+	getopt.showHelp();
+}
 
 var app = Shaft.create({
-	mode        : 'development',
+	mode : 'development',
+	host : options.host || 'localhost',
+	port : options.port || 8000,
 	dirs : {
 		root  : __dirname,
 		views : 'views'
