4041a6bd4db0eca28d24ef3225a31858814e2103
rumkin <hello@rumk.in>
2013-06-25 12:48:38 +0400

    Example comments and cli runmode support  added

--- examples/default/app.js
+++ examples/default/app.js
@@ -6,6 +6,7 @@ var getopt, options;
 getopt = new Getopt([
 	['H', 'host=ARG', 'hostname or ip'],
 	['p', 'port=ARG', 'port number'],
+	['m', 'mode=ARG', 'run mode'],
 	['h', 'help', 'show help']
 ]).bindHelp();
 
@@ -16,25 +17,39 @@ if (options.help) {
 	getopt.showHelp();
 }
 
+// Create application
 var app = Shaft.create({
-	mode : 'development',
+	mode : options.mode || 'development',
 	host : options.host || 'localhost',
 	port : options.port || 8000,
 	dirs : {
 		root  : __dirname,
 		views : 'views'
+	},
+	services : {
+		jade : true
 	}
 });
 
+// Binding jade service initialization event
 app.on('jade.init', function(jade) {
 	console.log('Jade is ready!');
 });
 
-app.start({
-	development : function(app, express) { },
-	production  : function(app, express) { }
-});
-
+// Error event listener binding
 app.on('error', function(err) {
 	console.error('Error', err);
+});
+
+// Start server
+app.start({
+	// Configure server in development mode
+	development : function(app, express) {
+		console.log('Configure express for development mode')
+	},
+
+	// Configure server in production mode
+	production  : function(app, express) {
+		console.log('Configure express for production mode')
+	}
 });
